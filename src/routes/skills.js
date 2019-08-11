181f63f170d617463090f2c6db9e5a4282b40ea2
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-08-11 08:17:54 -0500

    CRUD for skills

--- src/routes/skills.js
+++ src/routes/skills.js
@@ -31,9 +31,48 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', auth, async (req, res) => {
+    logger.log({
+        level: 'info',
+        message: 'Processing post request...',
+    });
     const agent = new https.Agent({rejectUnauthorized: false});
-    const result = await axios.get(api, {httpsAgent: agent});
+    const result = await axios.post(api, req.body, {httpsAgent: agent});
+    logger.log({
+        level: 'info',
+        message: 'Data for post request was successfully saved: ',
+        meta: JSON.stringify(result.data),
+    });
     return res.send(result.data);
+});
+
+router.patch('/', auth, async (req, res) => {
+    const agent = new https.Agent({rejectUnauthorized: false});
+    logger.log({
+        level: 'info',
+        message: 'Processing patch request...',
+    });
+    const result = await axios.patch(api, req.body, {httpsAgent: agent});
+    logger.log({
+        level: 'info',
+        message: 'Data for patch request was successfully updated: ',
+        meta: JSON.stringify(result.data),
+    });
+    return res.send(result.data);
+});
+
+router.delete('/:id', auth, async (req, res) => {
+    const agent = new https.Agent({rejectUnauthorized: false});
+    logger.log({
+        level: 'info',
+        message: 'Processing delete request...',
+    });
+    const result = await axios.delete(api + req.params.id, {httpsAgent: agent});
+    logger.log({
+        level: 'info',
+        message: 'Data for delete request was successfully processed: ',
+        meta: JSON.stringify(result.data),
+    });
+    return res.sendStatus(result.data);
 });
 
 module.exports = router;
